49229fc71a519e59fa13d28bb6cca06a4945aa90
Leo Goršić <gorsicleo@gmail.com>
2022-02-06 15:17:46 +0100

    Finish login screen design

---
+++ TV Shows/TV Shows/Common/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  TV Shows
+//
+//  Created by Leo Goršić on 05.02.2022..
+//
+
+import Foundation
+import UIKit
+
+enum Constants {
+    
+    // MARK: - Buttons -
+    
+    enum Buttons {
+        static let visibilityEnabled = UIImage(named: "VisibilityIconEnabled")
+        static let visibilityDisabled = UIImage(named: "VisibilityIconDisabled")
+        static let rememberMeButtonUnchecked = UIImage(named: "ic-checkbox-unselected")
+        static let rememberMeButtonChecked = UIImage(named: "ic-checkbox-selected")
+    }
+    
+    // MARK: - Colors-
+    
+    enum Colors {
+        static let mainRedColor = UIColor(rgb: 0x800000)
+        static let disabledMainColor = UIColor(rgb: 0xaf4141)
+        static let disabledTitleColor = UIColor(rgb: 0xcccccc)
+        
+    }
+}
--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -10,10 +10,13 @@ final class LoginViewController: UIViewController {
     
     // MARK: - Private Properties -
     
-    private var numberOfClicks = 0
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var rememberMeButton: UIButton!
+    @IBOutlet private weak var passwordVisibilityButton: UIButton!
+    @IBOutlet private weak var loginButton: CustomButton!
+    @IBOutlet private weak var registerButton: UIButton!
     
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var flashingColorView: UIView!
     
     // MARK: - ViewController Life Cycle
     
@@ -21,6 +24,7 @@ final class LoginViewController: UIViewController {
         super.viewDidLoad()
         setUpUI()
     }
+    
 }
 
 // MARK: - Extensions -
@@ -30,26 +34,98 @@ final class LoginViewController: UIViewController {
 private extension LoginViewController {
     
     func setUpUI() {
-        setUpTitle()
-        setUpView()
+        setUpTextFields()
+        setUpPasswordVisibilityButton()
+        setUpRememberMeButton()
+        setUpLoginButton()
     }
     
-    func setUpTitle() {
-        titleLabel.text = String(numberOfClicks)
+    func setUpTextFields() {
+        setUpEmailTextField()
+        setUpPasswordTextField()
     }
     
-    func setUpView() {
-        flashingColorView.backgroundColor = UIColor.cyan
+    
+    func setUpEmailTextField() {
+        setWhiteTextColor(for: emailTextField, with: "Email")
+        removeTextFieldBorder(for: emailTextField)
     }
+    
+    func setUpPasswordTextField() {
+        setWhiteTextColor(for: passwordTextField, with: "Password")
+        removeTextFieldBorder(for: passwordTextField)
+    }
+    
+    func removeTextFieldBorder(for textField: UITextField) {
+        textField.borderStyle = .none
+    }
+    
+    func setWhiteTextColor(for textField: UITextField, with placeholderText: String) {
+        textField.textColor = .white
+        textField.attributedPlaceholder = NSAttributedString(
+            string: placeholderText,
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+    }
+    
+    
+    func setUpRememberMeButton() {
+        rememberMeButton.setImage(Constants.Buttons.rememberMeButtonUnchecked, for: .normal)
+        rememberMeButton.setImage(Constants.Buttons.rememberMeButtonChecked, for: .selected)
+    }
+    
+    func setUpPasswordVisibilityButton() {
+        passwordVisibilityButton.setImage(Constants.Buttons.visibilityEnabled, for: .normal)
+        passwordVisibilityButton.setImage(Constants.Buttons.visibilityDisabled, for: .selected)
+    }
+    
+    func setUpLoginButton() {
+        loginButton.isEnabled = false
+        loginButton.layer.cornerRadius = 18
+        setUpLoginButtonBackgroundColor()
+        setUpLoginButtonTextColor()
+    }
+    
+    func setUpLoginButtonBackgroundColor() {
+        loginButton.setBackgroundColor(.white, for: .normal)
+        loginButton.setBackgroundColor(Constants.Colors.disabledMainColor, for: .disabled)
+    }
+    
+    func setUpLoginButtonTextColor() {
+        loginButton.setTitleColor(Constants.Colors.disabledTitleColor, for: .disabled)
+        loginButton.setTitleColor(Constants.Colors.mainRedColor, for: .normal)
+    }
+
+    
+    func checkInputValidity() {
+        guard let emailFieldInput = emailTextField.text else {return}
+        guard let passwordFieldInput = passwordTextField.text else {return}
+
+        loginButton.isEnabled = emailFieldInput.count > 0 && passwordFieldInput.count > 0 ? true : false
+    }
+    
+    
 }
 
 // MARK: - IBAction -
 
 private extension LoginViewController {
     
-    @IBAction func incrementAction() {
-        numberOfClicks += 1
-        titleLabel.text = String(numberOfClicks)
-        flashingColorView.backgroundColor =  numberOfClicks % 2 == 0 ? .magenta : .cyan
+    @IBAction func passwordVisibilityAction() {
+        passwordTextField.isSecureTextEntry.toggle()
+        passwordVisibilityButton.isSelected = !passwordVisibilityButton.isSelected
+    }
+    
+    // Iz nekog razloga baca exception kad funkcija ne prima sendera
+    @IBAction func rememberMe(_ sender: UIButton) {
+        rememberMeButton.isSelected = !rememberMeButton.isSelected
+    }
+    
+    @IBAction func emailFieldValueChanged(_ sender: Any) {
+        checkInputValidity()
+    }
+    
+    @IBAction func passwordFieldValueCahnged(_ sender: Any) {
+        checkInputValidity()
     }
 }
